a8c5765e6a3dcafd01c144011e3206264595b38b
huangzx <hzhixi9@gmail.com>
2021-06-20 16:02:06 +0800

    snabbdom 环境搭建

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**开发模式 */
   mode: "development",
   /**入口 */
-  entry: "./mustache/index.js",
+  entry: "./profit/index.js",
   /**打包到什么文件 */
   output: {
     filename: "bundle.js",
@@ -16,7 +16,7 @@ module.exports = {
     /**不压缩 */
     compress: false,
     /**端口号 */
-    port: 8801,
+    port: 8802,
     /**虚拟打包的路径，bundle.js文件没有真正生成 */
     publicPath: "/xuni/",
   },
